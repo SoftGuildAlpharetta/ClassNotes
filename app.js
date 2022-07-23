8fed53d7815e9dc7ff9ab843bf613db31d500222
Jonathan Ling <jonathanling@shamrocksweb.com>
2022-07-23 11:27:25 -0400

    Add basic guestbook app.

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
-var me = "Jonathan";
-var meBirthYear = 1992;
-console.log("hello world");
-console.log(`My name is ${me}!`);
-console.log(`I was born in ${meBirthYear} AD!`);
-// const promptOutputValue = prompt("Hey there, what's your name?");
-// console.log(`Username is: ${promptOutputValue}`);
-// if (promptOutputValue === "David") {
-//   alert(
-//     `Hello ${promptOutputValue}, I've concluded the only winning move is not to play.`
-//   );
-// } else {
-//   alert(
-//     `Hello ${promptOutputValue}, welcome to my program, would you like to play a game?`
-//   );
-// }
+const BIN_URL = "https://api.jsonbin.io/v3/b/62dc0c482c868775a534ada7";
+fetch(BIN_URL).then((res) =>
+  res.json().then((json) => {
+    const guests = json.record || [];
+    const element = document.getElementById("guest-book");
+    const submitBtn = document.getElementById("submit-btn");
+    const innerGuestList = document.getElementById("guest-book-list");
+    const listOfGuests = guests
+      .map((guest) => `<li>${guest.name} - ${guest.birthday}</li>`)
+      .join("");
+    innerGuestList.innerHTML = listOfGuests;
+
+    const submitForm = (event) => {
+      event.preventDefault();
+      const nameField = document.getElementById("name-input");
+      const bDayField = document.getElementById("bday-input");
+      guests.unshift({ name: nameField.value, birthday: bDayField.value });
+      const listOfGuests = guests
+        .map((guest) => `<li>${guest.name} - ${guest.birthday}</li>`)
+        .join("");
+      innerGuestList.innerHTML = listOfGuests;
+      nameField.value = "";
+      bDayField.value = "";
+      const requestObject = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Access-Key":
+            "$2b$10$ywKZCpz8UFYNO6lEpUSw7.ihEaDdRK8S2y8gSFSeuUjQtDSFMRvmq",
+        },
+        body: JSON.stringify(guests),
+      };
+      fetch(BIN_URL, requestObject).then((res) => res.json().then(console.log));
+    };
+
+    submitBtn.addEventListener("click", submitForm);
+  })
+);
